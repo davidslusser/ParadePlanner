6c7c1abcc5e4cfce37fce4b8f8664c8d96d3e25d
David Slusser <david.slusser@sealstorage.io>
2024-10-21 16:38:11 -0700

    testing deployment to render

---
+++ src/django_project/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+pwd
+python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py add_superuser --username admin --group admin --password $ADMINPWD
+gunicorn --workers 3 --bind 0.0.0.0:8000 core.wsgi:application
