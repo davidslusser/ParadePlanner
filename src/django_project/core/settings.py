977b86a681fc53dee9650288754f92b39cf1645d
David Slusser <david.slusser@sealstorage.io>
2024-10-21 17:32:14 -0700

    testing render deployment

--- src/django_project/core/settings.py
+++ src/django_project/core/settings.py
@@ -72,16 +72,20 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "handyhelpers.middleware.RequireLoginMiddleware",
     "userextensions.middleware.UserRecentsMiddleware",
 ]
+
+if DEBUG:
+    MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
+
 
 ROOT_URLCONF = "core.urls"
 
