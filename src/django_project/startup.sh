5bbf95a515d1943f228024b32870e51753a83dc7
David Slusser <david.slusser@sealstorage.io>
2024-10-21 17:50:15 -0700

    update gunicorn command

--- src/django_project/startup.sh
+++ src/django_project/startup.sh
@@ -3,4 +3,4 @@ pwd
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py add_superuser --username admin --group admin --password $ADMINPWD
-gunicorn --workers 3 --bind 0.0.0.0 core.wsgi:application
+gunicorn core.wsgi
